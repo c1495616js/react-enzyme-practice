a8bc96c89c2f61a84871c24e9461c9bf32ed05ca
Jerry Wang <jerryappleid761208@gmail.com>
2019-06-27 21:50:54 -0700

    beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+})
+
+it('shows a comment box', () => {  
   expect(wrapped.find(CommentBox).length).toEqual(1);
+})
+
+it('show a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 })
